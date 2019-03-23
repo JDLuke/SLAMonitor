a7f937c566660e2636b06c227fe219951ca63d11
John Luke <John.Luke@cognizant.com>
2019-03-23 10:32:05 -0400

    Getting multi-project gradle into better shape, adding structure for RESTful app to be monitored.

---
+++ applications/monitored-app/src/main/java/com/jdluke/sla_montor/monitored_app/MonitoredApplication.java
@@ -0,0 +1,7 @@
+package com.jdluke.sla_montor.monitored_app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoredApplication {
+}
